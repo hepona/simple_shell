0605e3c1e9d589cba21b7fc83d1393efd702d92d
Anas <anasjelloule@gmail.com>
2023-05-11 11:15:52 -0700

    Create Main File

---
+++ shell.c
@@ -0,0 +1,10 @@
+/**
+ * main - check the code
+ * @a_c: The number of arguments supplied to the program.
+ * @a_v: An array of pointers to the arguments.
+ * Return: Always 0.
+ */
+int main(int a_c, char *a_v[])
+{
+return (0);
+}
