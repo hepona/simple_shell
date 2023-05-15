320aad7b7c0897f9e9000c795deb7ef3239fab9d
hepona <hepona10@gmail.com>
2023-05-15 08:44:35 -0700

    main.c BETTY : OK

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * main -> simple shell
+ * @argv: arguments
+ * @ac: num of arguments
+ * Return: 0
+ */
+int main(__attribute__((unused)) int ac, char **argv)
+{
+	char *cmd = NULL, *cmd_cp = NULL;
+	size_t bufsize = 0;
+	ssize_t cmdread;
+	char *token,  **arg;
+	int i, c_tok = 0;
+
+	while (1)
+	{
+		_putstr("($) > ");
+		cmdread = getline(&cmd, &bufsize, stdin);
+		cmd_cp = malloc(sizeof(char *)  * cmdread + 1);
+		if (cmd_cp == NULL)
+		{
+			perror("Error: malloc");
+			return (-1);
+		}
+		strcpy(cmd_cp, cmd);
+		token = strtok(cmd, " \n");
+		while (token != NULL)
+		{
+			c_tok++;
+			token = strtok(NULL, " \n");
+		}
+		c_tok++;
+		arg = malloc(sizeof(char *) * c_tok);
+		token = strtok(cmd_cp, " \n");
+		for (i = 0; token != NULL; i++)
+		{
+			arg[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(arg[i], token);
+			token = strtok(NULL, " \n");
+		}
+		arg[i] = NULL;
+
+		execute_cmd(arg);
+		free(cmd_cp);
+		free(arg);
+	}
+	free(cmd);
+	return (0);
+}
