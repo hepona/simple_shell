28080c40e6eac3b504ae1514bf1cb7ee6e5a24c2
hepona <hepona10@gmail.com>
2023-05-15 09:37:58 -0700

    execute_cmd file BETTY : OK

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,48 +1,25 @@
 #include "shell.h"
 
 /**
- * execute_cmd -> execute a command entered by user
- * @cmd: commande entered
-*/
-
+ * execute_cmd -> execute command from user
+ * @argv: command entered by user
+ */
 void execute_cmd(char **argv)
-{ 
-    char *cmd = NULL;
-    int pid = fork();
+{
+	char *cmd = NULL;
+	int pid = fork();
 
-    if (pid ==  0){
-       
-        if (argv != NULL) {
-            cmd = argv[0];
-            if (execve(cmd, argv, NULL) == -1){
-                perror("Error");
-            }
-        }
-    }
-    else if (pid == -1)
-    {
-        perror("Error");
-        exit (1);
-    }
-    else
-        wait(NULL);
-    
-    
-
-
-    //     cmd = argv[0];
-    // // return (pid == -1)? -1 : 0;
-    // if (pid == -1)
-    //     return -1;
-    // if (pid == 0 && execve("cmd", argv, NULL))
-    // {   
-    //     if (exe == -1)
-    //       perror("Error : execve");
-    // }
-    // else
-    //   wait(NULL);
-
-
-
-
+	switch (pid)
+	case 0:
+		if (argv != NULL)
+		{
+			cmd = argv[0];
+			if (execve(cmd, argv, NULL) == -1)
+				perror("Error");
+		}
+	case -1:
+		perror("Error");
+		exit(1);
+	default:
+		wait(NULL);
 }
