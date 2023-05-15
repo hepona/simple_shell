148703b3082951d86d30c7b4de827de870542569
Anas <anasjelloule@gmail.com>
2023-05-15 10:06:44 -0700

    Update env and refrsh base

--- str_anas_helper.c
+++ str_anas_helper.c
@@ -17,6 +17,43 @@ str_l++;
 return (cn);
 }
 /**
+ * str_concat - concatenates two strings.
+ * @st1: 1 string
+ * @st2: 2 string
+ *
+ * Return: Pointer to new string,if Failure NULL
+ */
+char *str_concat(char *st1, char *st2)
+{
+int cn = _strlen(st1) + _strlen(st2);
+char *t_o = malloc(cn + 1);
+char *frm = t_o;
+
+if (st1 != NULL)
+{
+while (*st1 != '\0')
+{
+*frm = *st1;
+frm++;
+st1++;
+}
+}
+
+if (st2 != NULL)
+{
+while (*st2 != '\0')
+{
+*frm = *st2;
+frm++;
+st2++;
+}
+}
+
+*frm = '\0';
+
+return (t_o);
+}
+/**
  *  str_comp - Compares two strings.
  * @st1: string
  * @st2: string
@@ -33,6 +70,24 @@ st2++;
 if (*st1 != *st2)
 return (*st1 - *st2);
 return (0);
+}
+/**
+ * _strchr - check a character in a string.
+ * @st: The string to be searched.
+ * @c: The character to be check.
+ *
+ * Return: a pointer to the first occurence if c was found otherwise return Null.
+ */
+char *_strchr(char *st, char c)
+{
+int cn;
+
+for (cn = 0; st[cn]; cn++)
+{
+if (st[cn] == c)
+return (st + cn);
+}
+return (NULL);
 }
 /**
  * strn_comp - Compares two strings.
