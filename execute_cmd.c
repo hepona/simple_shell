663ce065f95f0912098ee1b27b95b9c872aa186e
hepona <hepona10@gmail.com>
2023-05-20 02:54:05 +0100

    verify cmd before fork

--- execute_cmd.c
+++ execute_cmd.c
@@ -3,19 +3,27 @@
 /**
  * execute_cmd -> execute command from user
  * @argv: command entered by user
- * @filename: name of the executable file
+ * @filename: name of the executed file
  */
 void execute_cmd(char **argv, char *filename)
 {
-	char *cmd = NULL;
-	int pid = fork();
+	char *cmd = str_concat("/bin/", argv[0]);
+	int pid = 0;
+	char *path = _getenv("PATH");
 
+	if (access(cmd, X_OK))
+	{
+		perror(filename);
+		return;
+	}
+	pid = fork();
 	if (pid == 0)
 	{
-	  cmd =str_concat("/bin/", argv[0]);
-			if (execve(cmd, argv, NULL) == -1)
-				perror(filename);
-			 exit(EXIT_FAILURE);
+		if (execve(cmd, argv, NULL) == -1)
+		{
+			perror(filename);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else if (pid == -1)
 	{
