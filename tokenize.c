cf7a4ac28fb104c07927b643118a25d49da1359c
hepona <hepona10@gmail.com>
2023-05-21 03:39:17 +0100

    tokenize func

---
+++ tokenize.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * tokenize -> tokenize a string
+ * @str: string that will be tokenized
+ * @delimiter: delimiter
+ * Return: number of tokens
+*/
+int tokenize(char *str, char *delimiter)
+{
+int c_token = 0;
+char *token;
+
+token = strtok(str, delimiter);
+while (token != NULL)
+{
+c_token++;
+token = strtok(NULL, delimiter);
+}
+c_token++;
+return (c_token);
+}
