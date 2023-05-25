df564f3042165c9708c3a87a76ef3defb1b197bf
hepona <hepona10@gmail.com>
2023-05-25 18:06:45 +0100

    test meo leak

--- path.c
+++ path.c
@@ -2,26 +2,32 @@
 
 /**
  * file_path -> localisation of the path
- * @cmd: command
- * Return: the whole command
+ * @cmd: command
+ * Return: the whole command
 */
 
 char *file_path(char *cmd)
 {
-char *path, *path_cp, *tok, *dir;
+char *path, *path_cp, *tok, *dir, *tmp;
 struct stat buf;
 path = _getenv("PATH");
 if (path == NULL)
 return (NULL);
-path_cp = strdup(path);
+path_cp = _strdup(path);
 tok = strtok(path_cp, ":");
 while (tok != NULL)
 {
 dir = malloc(_strlen(cmd) + _strlen(tok) + 2);
 _strcpy(dir, tok);
-dir = str_concat(dir, "/");
-dir = str_concat(dir, cmd);
-dir = str_concat(dir, "\0");
+tmp = str_concat(dir, "/");
+free(dir);
+dir = tmp;
+tmp = str_concat(dir, cmd);
+free(dir);
+dir = tmp;
+tmp = str_concat(dir, "\0");
+free(dir);
+dir = tmp;
 if (stat(dir, &buf) == 0)
 {
 free(path_cp);
@@ -34,7 +40,10 @@ tok = strtok(NULL, ":");
 }
 }
 if (stat(cmd, &buf) == 0)
+{
+free(path_cp);
 return (cmd);
+}
 free(path_cp);
 return (cmd);
 }
